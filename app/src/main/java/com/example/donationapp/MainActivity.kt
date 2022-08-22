62a41daa5f4cb4e5691e3a149afe7e13ed87e085
Deivid Gomes <davidsilva29@hotmail.com>
2022-08-22 14:51:09 -0300

    Adição de recuperação de senha

--- app/src/main/java/com/example/donationapp/MainActivity.kt
+++ app/src/main/java/com/example/donationapp/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.donationapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.WindowManager
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
@@ -54,6 +57,26 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        //Detecção do clique no botão de "Esqueci a senha ?"
+
+        val textViewForgetPassword = findViewById<TextView>(R.id.textViewForgetPassword)
+        textViewForgetPassword.setOnClickListener {
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Recuperação de senha")
+            val view = layoutInflater.inflate(R.layout.dialog_forget_password,null)
+            val email = view.findViewById<EditText>(R.id.editTextEmailForgetPassword)
+            builder.setView(view)
+            builder.setPositiveButton("Enviar", DialogInterface.OnClickListener { _, _ ->
+                Log.i("Test",email.text.toString())
+                forgotPassword(email)
+            })
+            builder.setNegativeButton("Fechar", DialogInterface.OnClickListener { _, _ ->
+            })
+            builder.show()
+
+        }
+
         //Detecção de clique no texto de "Crie uma conta"
 
         val textViewLogin = findViewById<TextView>(R.id.textViewLogin)
@@ -63,6 +86,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+    }
+
+    private fun forgotPassword(email: EditText) {
+        if(email.text.toString().isEmpty()){
+            Toast.makeText(this,"O email não foi informado.", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(!(android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches())){
+            Toast.makeText(this,"O email está mal formatado.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val auth = FirebaseAuth.getInstance()
+        auth.sendPasswordResetEmail(email.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this,"Email de recuperação de senha enviado.", Toast.LENGTH_LONG).show()
+                    Log.i("Test", "Email recuperação enviado")
+                }
+            }
+
     }
 
     private fun signIn() {
